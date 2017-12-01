28a24da96a1a6ec3aeea831064bf9a9258805e7d
Rahul Ghosh <rahulghosh2021@gmail.com>
2017-12-01 17:49:36 +0530

    Update main.py

--- PYTHON_CODE/main.py
+++ PYTHON_CODE/main.py
@@ -19,24 +19,28 @@ def elitist_quantum_diff_evol():
     pop_obs_qubits = quantum_diff_evol.pop_observe(pop_qubits)
 
     print("Calculating Initial Accuracy")
-    qubits_accuracy = quantum_diff_evol.pop_accuracy(pop_obs_qubits)
+    qubits_accuracy, qubits_cross_val_score = quantum_diff_evol.pop_accuracy(pop_obs_qubits)
 
     for iter in range(0, config.EQDE_MAXITER):
         print("QDE iteration start")
         print("Iteration Number: ", iter)
+        print("Calculating Original Accuracy")
+        qubits_accuracy, qubits_cross_val_score = quantum_diff_evol.pop_accuracy(pop_obs_qubits)
         print("Mutating Population")
         pop_mut_qubits = quantum_diff_evol.pop_mutation(pop_qubits)
         print("Population crossover")
         pop_cross_qubits = quantum_diff_evol.pop_crossover(pop_qubits, pop_mut_qubits, qubits_accuracy)
         pop_obs_cross_qubits = quantum_diff_evol.pop_observe(pop_cross_qubits)
         print("Calculating Crossover Accuracy")
-        cross_qubits_accuracy = quantum_diff_evol.pop_accuracy(pop_obs_cross_qubits)
+        cross_qubits_accuracy, cross_qubits_cross_val_score = quantum_diff_evol.pop_accuracy(pop_obs_cross_qubits)
         print("Population Selection")
         pop_qubits, pop_obs_qubits = quantum_diff_evol.pop_selection(pop_qubits, pop_obs_qubits, qubits_accuracy, pop_cross_qubits, pop_obs_cross_qubits, cross_qubits_accuracy)
 
+    final_pop_qubits = pop_qubits
+    final_pop_obs_qubits = pop_obs_qubits
     print("Calculating Final Accuracy")
-    final_qubtis_accuracy = quantum_diff_evol.pop_accuracy(pop_obs_qubits)
-
+    final_qubtis_accuracy, final_qubits_cross_val_score = quantum_diff_evol.pop_accuracy(final_pop_obs_qubits)
+    quantum_diff_evol.print_output(final_pop_qubits, final_pop_obs_qubits, final_qubits_cross_val_score)
 
 if __name__ == "__main__":
     elitist_quantum_diff_evol()
